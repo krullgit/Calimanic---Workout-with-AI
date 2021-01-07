5670692946ddc3b13965b5c2b444eed35b0cb15e
Matthes Krull <krull.matthes@googlemail.com>
2021-01-07 01:03:09 +0100

    deleted import

--- camera.js
+++ camera.js
@@ -17,6 +17,7 @@
 import * as posenet from '@tensorflow-models/posenet';
 import dat from 'dat.gui';
 import Stats from 'stats.js';
+//dwsd
 
 import {drawBoundingBox, drawKeypoints, drawSkeleton, isMobile, toggleLoadingUI, tryResNetButtonName, tryResNetButtonText, updateTryResNetButtonDatGuiCss} from './demo_util';
 
